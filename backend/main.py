cea44456e107e7c3c3341be32f4e457c6c8450fa
careagain <careagain.org@gmail.com>
2026-09-06 18:45:24 +0200

    fix: accessibility to admin portal

--- backend/main.py
+++ backend/main.py
@@ -304,4 +304,4 @@ def default_route():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    uvicorn.run(app, host="0.0.0.0", port=8888, forwarded_allow_ips="*")
